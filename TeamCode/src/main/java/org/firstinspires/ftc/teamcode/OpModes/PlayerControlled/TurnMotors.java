fd40ef5db92799541dd7083cf0f3d8e7d8c5fec8
Malte <malte.juenemann@igs-goettingen.de>
2020-11-12 17:26:53 +0100

    addded class new motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PlayerControlled/TurnMotors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PlayerControlled/TurnMotors.java
@@ -1,4 +1,24 @@
 package org.firstinspires.ftc.teamcode.OpModes.PlayerControlled;
 
-public class TurnMotors {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.HardwareMaps.BaseHardwareMap;
+import org.firstinspires.ftc.teamcode.HardwareMaps.SingleMotorHardwareMap;
+
+public class TurnMotors extends OpMode {
+    BaseHardwareMap peter;
+    @Override
+    public void init(){
+        peter = new SingleMotorHardwareMap(hardwareMap);
+    }
+
+    @Override
+    public void loop(){
+        if (gamepad1.a == true) {
+            peter.motor_front_right.setPower(1);                                                                               
+
+        }
+
+    }
 }
