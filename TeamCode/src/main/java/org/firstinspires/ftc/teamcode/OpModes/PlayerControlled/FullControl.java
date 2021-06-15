761fe0d4db30a72e85f3259d3773f7fc69edfe92
Simeon 'n0emis' Keske <git@n0emis.eu>
2021-06-15 15:56:55 +0200

    add example implementation for omniwheel

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PlayerControlled/FullControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PlayerControlled/FullControl.java
@@ -4,14 +4,17 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.HardwareMaps.BaseHardwareMap;
 import org.firstinspires.ftc.teamcode.HardwareMaps.FullHardwareMap;
+import org.firstinspires.ftc.teamcode.Tools.OmniWheel;
 
 @TeleOp
 public class FullControl extends OpMode {
     BaseHardwareMap robot;
+    OmniWheel omniwheel;
 
     @Override
     public void init() {
         robot = new FullHardwareMap(hardwareMap);
+        omniwheel = new OmniWheel(robot);
     }
 
     @Override
@@ -23,5 +26,7 @@ public class FullControl extends OpMode {
             robot.motor_flyingwheel_left.setPower(0);
             robot.motor_flyingwheel_right.setPower(0);
         }
+
+        omniwheel.setMotors(gamepad1.left_stick_y, gamepad1.left_stick_x, 0);
     }
 }
