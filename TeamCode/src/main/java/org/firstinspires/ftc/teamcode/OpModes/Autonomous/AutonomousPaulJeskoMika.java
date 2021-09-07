3a8e3ee132893fec3cc6ebbdba68743ec635375c
Malte <malte.juenemann@igs-goettingen.de>
2021-09-07 16:01:05 +0200

    add newest version for PaulJeskoMika

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/AutonomousPaulJeskoMika.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Autonomous/AutonomousPaulJeskoMika.java
@@ -7,27 +7,65 @@ import com.qualcomm.robotcore.hardware.CompassSensor;
 import org.firstinspires.ftc.teamcode.HardwareMaps.FullHardwareMap;
 import org.firstinspires.ftc.teamcode.Tools.ColorTools;
 import org.firstinspires.ftc.teamcode.Tools.OmniWheel;
+import org.firstinspires.ftc.teamcode.Tools.OrientationTools;
 
 @Autonomous
 public class AutonomousPaulJeskoMika extends LinearOpMode {
     FullHardwareMap robot;
     ColorTools colorTools;
     OmniWheel omniWheel;
+    OrientationTools orientationTools;
+    double startingAngle;
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot = new FullHardwareMap(hardwareMap);
         colorTools = new ColorTools();
         omniWheel = new OmniWheel(robot);
+        orientationTools = new OrientationTools(robot, this);
+        startingAngle = orientationTools.getDegree360(robot.imu);
 
         waitForStart();
 
 
-        omniWheel.setMotors(0.2, 0, 0);
+        omniWheel.setMotors(0.25, 0, 0.0055);
 
         while (!colorTools.isWhite(robot.colorSensor_left) && opModeIsActive()) {
+
+        }
+        omniWheel.setMotors(0, 0, 0);
+
+      /*  while (colorTools.isWhite(robot.colorSensor_front) && opModeIsActive()){
+            omniWheel.setMotors(0.25,0,0.005);
+            telemetry.addData("position", 2);
+            telemetry.update();
+
+            if (!colorTools.isWhite(robot.colorSensor_front)){
+                while (!colorTools.isWhite(robot.colorSensor_front) && !colorTools.isBlue(robot.colorSensor_front) && opModeIsActive()){
+                    omniWheel.setMotors(0,0,-0.2);
+                    telemetry.addData("position", 3);
+                    telemetry.update();
+
+                }
+            }
+            if (colorTools.isBlue(robot.colorSensor_front)){
+                telemetry.addData("Blau?",9);
+                telemetry.update();
+       */
+            }
+
+
         }
 
+
+        /*
+        Mika, bitte probier hier mal ein bisschen rum :)
+        */
+
+        //orientationTools.turnToDegrees(-90, 0.5, omniWheel, robot.imu );
+
+        //omniWheel.setMotors(0,0,0);
+/*
         if (!colorTools.isWhite(robot.colorSensor_left) && opModeIsActive()) {
 
 
@@ -36,12 +74,8 @@ public class AutonomousPaulJeskoMika extends LinearOpMode {
 
             }
         }
+*/
 
-        omniWheel.setMotors(0, -0.2, 0);
 
-        while (!colorTools.isBlue(robot.colorSensor_right) && opModeIsActive()) {
-        }
-        omniWheel.setMotors(0, 0, 0);
 
-    }
-}
+
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PlayerControlled/FullControl_PaulMikaJesko.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/PlayerControlled/FullControl_PaulMikaJesko.java
@@ -13,45 +13,48 @@ public class FullControl_PaulMikaJesko extends OpMode {
     BaseHardwareMap robot;
     OmniWheel omniwheel;
     ColorTools colorTools;
+    int x = 0;
 
     @Override
     public void init() {
         robot = new FullHardwareMap(hardwareMap);
         omniwheel = new OmniWheel(robot);
         colorTools = new ColorTools();
+
     }
 
     @Override
     public void loop() {
-        if (gamepad1.b) {
+        if (gamepad1.x) {
             robot.motor_flyingwheel_left.setPower(1);
             robot.motor_flyingwheel_right.setPower(-1);
-        } else if (gamepad1.a) {
+        } else if (gamepad1.b) {
             robot.motor_flyingwheel_left.setPower(0);
             robot.motor_flyingwheel_right.setPower(0);
         }
-        if(gamepad1.left_trigger>0){
-            robot.motor_front_left.setPower(-gamepad1.left_trigger);
-            robot.motor_front_right.setPower(gamepad1.left_trigger);
-            robot.motor_rear_left.setPower(-gamepad1.left_trigger);
-            robot.motor_rear_right.setPower(gamepad1.left_trigger);
-        }
-        if(gamepad1.right_trigger>0){
-            robot.motor_front_left.setPower(gamepad1.right_trigger);
-            robot.motor_front_right.setPower(-gamepad1.right_trigger);
-            robot.motor_rear_left.setPower(gamepad1.right_trigger);
-            robot.motor_rear_right.setPower(-gamepad1.right_trigger);
-        }
-        if(gamepad1.x){
-            robot.motor_gripper.setPower(1);
-        }
-        robot.motor_gripper.setPower(0);
-        if(gamepad1.y){
-            robot.servo_conveyor.setPosition(1);
-        }
-        robot.motor_gripper.setPower(0);
 
-        omniwheel.setMotors(gamepad1.left_stick_y, gamepad1.left_stick_x, 0);
+        if (x==0){
+            robot.servo_collector.setPosition(0.7);
+            x++;
+        }
+
+      if(gamepad1.dpad_up){
+          robot.motor_collector.setPower(1);
+      }
+
+      if(gamepad1.dpad_down){
+            robot.motor_collector.setPower(0);
+      }
+
+      if (gamepad1.dpad_left){
+            robot.servo_conveyor.setPosition(0);
+      }
+
+      if (gamepad1.dpad_right) {
+            robot.servo_conveyor.setPosition(0.5);
+      }
+            omniwheel.setMotors(-gamepad1.left_stick_y*0.66, -gamepad1.left_stick_x*0.66, gamepad1.left_trigger-gamepad1.right_trigger);
+
 
         telemetry.addData("weiss:", colorTools.isWhite(robot.colorSensor_right));
         float[] hsv = colorTools.showHSV(robot.colorSensor_right);
@@ -62,16 +65,19 @@ public class FullControl_PaulMikaJesko extends OpMode {
         telemetry.update();
 
         if (gamepad1.right_bumper){
-            robot.servo_gripper.setPosition(1);
+            robot.servo_gripper.setPosition(0.9);
         }
         if (gamepad1.left_bumper){
-            robot.servo_gripper.setPosition(0);
+            robot.servo_gripper.setPosition(0.6);
         }
-        if (gamepad1.dpad_up){
-            robot.motor_gripper.setPower(1);
+        if (gamepad1.a){
+            robot.motor_gripper.setPower(0.2);
         }
-        if (gamepad1.dpad_down){
-            robot.motor_gripper.setPower(-1);
+        if (gamepad1.y){
+            robot.motor_gripper.setPower(-0.2);
+        }
+        if (!gamepad1.a&&!gamepad1.y){
+            robot.motor_gripper.setPower(0);
         }
     }
 
